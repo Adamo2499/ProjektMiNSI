db455f88f64f22fea3358e44cc7c82d17060c536
Adamo2499 <30226946+Adamo2499@users.noreply.github.com>
2022-05-17 18:24:31 +0200

    Fix classes files

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ import classes.Miasta;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Miasta.DrukujOdleglosci();
     }
 }
