3c446e66fcde2b81618b9da6db2ce3e4e676e6d4
Talandar99 <grzegorzc66@gmail.com>
2022-05-17 19:27:32 +0200

    komentarz

--- src/classes/AE/AE.java
+++ src/classes/AE/AE.java
@@ -2,5 +2,11 @@ package classes.AE;
 
 public class AE {
     final static int MAXILOSCMIAST = 30;
+    // chromosom[iloscmiast]
+    // na chromosom składa się liczba genów równa ilości miast
+    // każdy gen musi być reprezentowany przez inną liczbę od 0 do liczbamiast
+    
+    // funkcja przystosowania danego chromosomu to suma drogi między wszystkimi miastami w kolejności wskazanej przez geny chromosomu
+    // optymalizacja polega na znalezieniu jak najkrótszej drogi pozwalającej na przebycie wszystkich wskazanych miast
     
 }
